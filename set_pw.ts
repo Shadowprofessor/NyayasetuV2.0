9663bb9012d05d28797f98ab32c8120f80bd7ee0
Shadowprofessor <sachitsharma802@gmail.com>
2026-03-27 03:04:55 +0530

    fixed the errors

--- set_pw.ts
+++ set_pw.ts
@@ -8,11 +8,7 @@ import { createClient } from '@supabase/supabase-js';
 
 const SALT_ROUNDS = 12;
 
-<<<<<<< Updated upstream
-const supabaseUrl = process.env.SUPABASE_URL!;
-=======
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
->>>>>>> Stashed changes
 const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY!;
 
 const supabase = createClient(supabaseUrl!, supabaseServiceKey!, {
